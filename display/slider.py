77bd7db29d4581f2632c01d13af4a3dff4b3c9ab
Miguel Tamayo <miiguell098@gmail.com>
2024-01-06 12:39:41 -0800

    finished slider

--- display/slider.py
+++ display/slider.py
@@ -27,41 +27,68 @@ class Slider(QWidget):
         slider (Slider): PyQt5 slider widget
     """
     valueChangedSignal = pyqtSignal()
+    internal_max = 100
+    internal_min = -100
     def __init__(self,
                  label: str,
                  min_val: int,
                  max_val: int,
                  width: int,
                  init_val: int = 0,
-                 tick_interval: int = 2,
                  orientation: int = 1) -> None:
         super().__init__(parent=None)
+
+        self.init_val = init_val
+        self.ratio = (max_val - min_val) / (self.internal_max - self.internal_min)
+        self.value = init_val
 
         layout = QHBoxLayout() # want a horizontal layout
 
         label = QLabel(label) # slider's label
-        self.value = QLabel(f"{init_val}") # slider's value
+        self.value_txt = QLabel(f"{init_val}") # slider's value
 
         # slider and alider attributes
         self.slider = QSlider()
         self.slider.setOrientation(orientation)
-        self.slider.setRange(min_val, max_val)
-        self.slider.setTickInterval(tick_interval)
+        self.slider.setRange(self.internal_min, self.internal_max)
         self.slider.setMaximumWidth(width)
 
         # add the items to the layout
         layout.addWidget(label)
         layout.addWidget(self.slider)
-        layout.addWidget(self.value)
+        layout.addWidget(self.value_txt)
 
         # set the widget's layout
         self.setLayout(layout)
 
-        self.slider.valueChanged.connect(self.valueChanged)
+        self.slider.valueChanged.connect(self.value_changed)
 
-    def valueChanged(self, value: int) -> None:
+    def value_changed(self, value: int) -> None:
         """
-        Changes the displayed value on the slider and emmits a signal for any other outside updates        
+        Changes the displayed value on the slider and emmits a signal for any other outside updates
+
+        inputs:
+        -------
+            value (int): new slider value 
         """
-        self.value.setText(str(value))
+        mapped_value = float(value) * self.ratio
+        self.value = mapped_value
+        self.value_txt.setText("{:.2f}".format(mapped_value))
         self.valueChangedSignal.emit()
+
+    def reset_slider(self) -> None:
+        """
+        Sets slider to the original position
+        """
+        self.slider.setValue(self.init_val)
+
+    def get_slider_value(self) -> float:
+        """
+        wrapper function to get the slider's value
+
+        return:
+        -------
+            value (float): slider's mapped value        
+        """
+
+        return float(self.slider.value()) * self.ratio
